75ea8b8234851d8ea05ef83126e2337be2f689ef
dkwkehgud77 <75433303+dkwkehgud77@users.noreply.github.com>
2022-01-06 21:29:38 +0900

    Update note.js

--- note.js
+++ note.js
@@ -1,1 +1,17 @@
+npm init -y
+npm i -D webpack webpack-cli webpack-dev-server@next
 
+package.json
+{
+  "name": "webpack-template-basic",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "dev": "webpak-dev-server --mode development",
+    "build": "webpack --mode production"  
+ }
+
+  
+  
+  
