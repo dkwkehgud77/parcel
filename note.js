0b1c55e674e9b4bedda6172019f673b79a366f67
dkwkehgud77 <75433303+dkwkehgud77@users.noreply.github.com>
2022-01-06 21:32:31 +0900

    Update note.js

--- note.js
+++ note.js
@@ -1,7 +1,7 @@
 npm init -y
 npm i -D webpack webpack-cli webpack-dev-server@next
 
-package.json
+vi package.json
 {
   "name": "webpack-template-basic",
   "version": "1.0.0",
@@ -12,6 +12,19 @@ package.json
     "build": "webpack --mode production"  
  }
 
-  
-  
+vi webpack.config.js
+module.exports = {
+    // 파일을 읽어들이기 시작하는 진입점 설정 
+    entry: './js/main.js',
+    
+    // 결과물(번들)을 반환하는 설정 
+    output: {
+        // path: path.resolve(__dirname, 'dist'),
+        // filename: 'main.js',
+        clean: true
+    }
+}
+
+npm run build 
+
   
